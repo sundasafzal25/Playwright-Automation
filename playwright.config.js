2aeefa2a18d8d0bea2843278da1d195d08fe92c0
Sundas Afzal <sundasafzal25@gmail.com>
2026-04-26 16:35:26 +0500

    ci: update main.yml with allure support and latest actions

--- playwright.config.js
+++ playwright.config.js
@@ -13,7 +13,7 @@ import { defineConfig, devices } from '@playwright/test';
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: './tests/e2e',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -23,15 +23,14 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-   reporter: [["line"], ["allure-playwright"]],
+   reporter: [['html'], ["line"], ["allure-playwright"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-  },
+  use: {
+  trace: 'on-first-retry',
+  headless: true, // This ensures tests run in the background
+  screenshot: 'only-on-failure', // Bonus: automatically takes a screenshot if a test fails
+},
 
   /* Configure projects for major browsers */
   projects: [
